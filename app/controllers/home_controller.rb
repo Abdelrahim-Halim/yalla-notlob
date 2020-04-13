e20f6f1e02f1f6dc72c1e49dec1b303b030e9bd5
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-04-13 07:30:57 +0200

    Create initial design of addFriend Page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def addFriend
+    render "home/addFriend"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :order
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/add_friend/:id' to: 'home#addFriend'
+  get 'home/add_friend', to: 'home#addFriend'
+
 end
