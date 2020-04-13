7ebceb16ffa029a35422aafa79eff3130e2b5843
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-04-13 09:35:31 +0200

    Create initial design of Group Page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/add_friend/:id' to: 'home#addFriend'
   get 'home/add_friend', to: 'home#addFriend'
+  get '/home/add_group', to: 'home#addGroup'
 
 end
