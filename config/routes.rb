a75dbca2e7558104509cf2532c634edbc1499fe0
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-04-13 07:30:57 +0200

    Create initial design of addFriend Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/add_friend/:id' to: 'home#addFriend'
+  get 'home/add_friend', to: 'home#addFriend'
+
 end
