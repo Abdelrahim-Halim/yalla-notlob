39a37cb87c0623e8f527bfcbeb12df82c8c6285c
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-04-14 10:07:06 +0200

    create addNewFriend function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   devise_for :users
   get 'home/index'
   root 'home#index'
   resources :order
+||||||| merged common ancestors
+  get 'home/index'
+  root 'home#index'
+=======
+  get 'home/index', to:'home#index'
+  root to: 'home#index'
+>>>>>>> create addNewFriend function
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/add_friend/:id' to: 'home#addFriend'
   get 'home/add_friend', to: 'home#addFriend'
-  get '/home/add_group', to: 'home#addGroup'
+  get 'home/add_group', to: 'home#addGroup'
 
 end
