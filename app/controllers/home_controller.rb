2f98576972c50b0611162fa238e52246b9315792
mo7amedsalah <mohamed.s.abokhadra@gmail.com>
2020-04-19 18:16:11 +0200

    add logic of friends activities to home controler and homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,14 @@ class HomeController < ApplicationController
     @current_user = User.find(1)
     if current_user
       @orders = Order.all.where(user_id: current_user.id).order(created_at: :desc)
-      # @all = []
-      # current_user.friends.each { |fr|
-      #   @all.push fr.orders
-      # }
+      @all = []
+       current_user.friendships.each { |fr|
+         @all.push fr.orders
+       }
 
-      # puts "All"
-      # puts @all.inspect
-      # @allmyFriends = current_user.friends
+       puts "All"
+       puts @all.inspect
+       @allmyFriends = current_user.friendships
     else
       puts "sign_in"
       # redirect_to "/users/sign_in"
