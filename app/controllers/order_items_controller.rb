38209bf50e858ec4913124ff214ebf838ac43d97
halim <abdohalem91@gmail.com>
2020-04-19 22:51:15 +0200

    order details

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,34 @@
+class OrderItemsController < ApplicationController
+  def index
+    @order = Order.find(params[:id])
+    @current_user = User.find(1)
+    @orderDetails = @order.order_items
+  end
+
+  def create
+    @item = OrderItem.new
+    @item.item = params[:item]
+    @item.price = params[:price]
+    @item.quntity = params[:quntity]
+    @item.comment = params[:comment]
+    @item.order_id = params[:order_id]
+    @item.user_id = 1
+
+    puts (@item)
+    if  @item.save
+      redirect_to order_details_path(id: @item.order_id)
+    else
+      puts (@item.errors.full_messages)
+      redirect_to :root
+    end
+
+  end
+
+  def destroy
+    order_item = OrderItem.find(params[:id])
+    order_id = order_item.order_id
+    order_item.destroy
+    redirect_to order_details_path(id: order_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :order_items
+  get 'orders/:id/items', to: 'order_items#index', as: 'order_details' 
+  post 'items', to: 'order_items#create', as: 'add_item'
+  delete 'items/:id', to: 'order_items#destroy', as: 'destroy_item'
+
+  # get 'item/:id', to: 'order_items#destroy', as: 'destroy_item'
+  
   devise_for :users
   get 'home/index'
   root 'home#index'
