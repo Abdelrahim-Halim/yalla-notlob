8662bf0181f4bd0d8df0a745c3a67bbaf4cdedf0
MinaMaher0 <minamaher30697@gmail.com>
2020-04-20 23:45:08 +0200

    add notifications

--- db/migrate/20200412102059_create_users.rb
+++ db/migrate/20200412102059_create_users.rb
@@ -40,8 +40,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
 
       t.timestamps
     end
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    #add_index :users, :email,                unique: true
+    #add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -97,10 +97,13 @@ ActiveRecord::Schema.define(version: 2020_04_19_204433) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
-    t.string "password"
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
